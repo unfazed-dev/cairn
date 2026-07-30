f0f3986ad63309168ecf40b7bafcd62352404de1
Cairn Founder <founders@cairn.dev>
2026-07-31 01:41:40 +1000

    chore: remove superseded Flutter fixtures (greenfield per plan D0)

--- fixtures/flutter/pomodoro/android/app/src/main/kotlin/dev/cairn/pomodoro/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.cairn.pomodoro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- fixtures/flutter/todo/android/app/src/main/kotlin/dev/cairn/todo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.cairn.todo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
